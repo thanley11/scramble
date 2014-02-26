e31348bcf9b61ff5ac9fa229b5e3cc7deaa7fcbd
Tom Hanley <thanley11@gmail.com>
2014-02-26 13:54:08 -0500

    Fixed migrations problem (had extra 000X_initial.py that I had to delete), and updated models.py and admin.py to show course info

--- scramble/admin.py
+++ scramble/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from scramble.models import Course
 # Register your models here.
+class CourseAdmin(admin.ModelAdmin):
+    list_display =  ['name']
+
+admin.site.register(Course, CourseAdmin)
--- scramble/models.py
+++ scramble/models.py
@@ -9,9 +9,9 @@ class UserProfile(models.Model):
         
 class Course(models.Model):
 	name 		= models.CharField(max_length=128, unique=True)
-	#par  		= models.IntegerField(default=0)
-	#handicap 	= models.IntegerField(default=0)
-	#location 	= models.CharField(max_length=128)
+	par         = models.IntegerField(default=0)
+	handicap 	= models.IntegerField(default=0)
+	location 	= models.CharField(max_length=128, default=None, blank=True)
 	
 	def __unicode__(self):
 		return self.name
