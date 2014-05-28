5857ec41983b9be76ead22e1df526ada9657d84b
Tom Hanley <thanley11@gmail.com>
2014-05-28 19:16:02 +0000

    Removed wsgi.py

--- wsgi.py
+++
@@ -1,8 +0,0 @@
-import os, sys, site, django.core.handlers.wsgi
-
-SITE_DIR = '/home/vagrant/projects/scramble'
-site.addsitedir(SITE_DIR)
-sys.path.append(SITE_DIR)
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'scramble_project.settings'
-application = django.core.handlers.wsgi.WSGIHandler()
