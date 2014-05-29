5b37810dc30f24a26e542306f8655889099cb488
Tom Hanley <thanley11@gmail.com>
2014-05-29 16:35:02 +0000

    Updated settings removed key

--- scramble_project/settings.py
+++ scramble_project/settings.py
@@ -31,7 +31,7 @@ print "DB:", DATABASE_PATH
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '41)4w^%fs4(8i#+yfl1=uk6nl38p(@26hx+w$)+2o90^3hjr7p'
+SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
