5d78b3227bb773a3869027f26765c0dc4adef81f
Tom Hanley <thanley11@gmail.com>
2014-06-03 15:39:45 +0000

    Altered css - background color, hr

--- scramble/admin.py
+++ scramble/admin.py
@@ -2,6 +2,6 @@ from django.contrib import admin
 from scramble.models import Course
 # Register your models here.
 class CourseAdmin(admin.ModelAdmin):
-    list_display =  ['name']
+    list_display =  ['name','par','location']
 
 admin.site.register(Course, CourseAdmin)
--- scramble_project/settings.py
+++ scramble_project/settings.py
@@ -34,7 +34,7 @@ print "DB:", DATABASE_PATH
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
