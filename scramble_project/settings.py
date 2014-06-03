d739ac131814ef70206b5ec366c579f26973c8cf
Tom Hanley <thanley11@gmail.com>
2014-06-03 14:51:05 +0000

    Changed about page

--- scramble_project/settings.py
+++ scramble_project/settings.py
@@ -34,7 +34,7 @@ print "DB:", DATABASE_PATH
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 TEMPLATE_DEBUG = True
 
